f3002cca1578f6be702aee18fe7ab713064be5e8
Hafijul Islam <hafijul233@gmail.com>
2023-01-15 02:34:55 +0600

    activity layout added

---
+++ app/src/main/java/com/hafijulislam/doggler/VerticalListActivity.kt
@@ -0,0 +1,11 @@
+package com.hafijulislam.doggler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VerticalListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vertical_list)
+    }
+}
