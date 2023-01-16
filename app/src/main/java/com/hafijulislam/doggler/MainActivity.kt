9e50c3c56674f27612edd9f08c8dfb0e562d36b6
Hafijul Islam <hafijul233@gmail.com>
2023-01-16 12:18:44 +0600

    vertical list style updated

--- app/src/main/java/com/hafijulislam/doggler/MainActivity.kt
+++ app/src/main/java/com/hafijulislam/doggler/MainActivity.kt
@@ -13,5 +13,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Inflate the layout XML file and return a binding object instance
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // Set the content view of the Activity to be the root view of the layout
+        setContentView(binding.root)
+
+        // Launch the VerticalListActivity on verticalBtn click
+        binding.verticalButton.setOnClickListener { launchVertical() }
+
+        // Launch the HorizontalListActivity on horizontalBtn click
+        binding.horizontalButton.setOnClickListener { launchHorizontal() }
+
+        // Launch the GridListActivity on gridBtn click
+        binding.gridButton.setOnClickListener { launchGrid() }
+    }
+
+    private fun launchVertical() {
+        listIntent = Intent(this, VerticalListActivity::class.java)
+        startActivity(listIntent)
+    }
+
+    private fun launchHorizontal() {
+        listIntent = Intent(this, HorizontalListActivity::class.java)
+        startActivity(listIntent)
+    }
+
+    private fun launchGrid() {
+        listIntent = Intent(this, GridListActivity::class.java)
+        startActivity(listIntent)
     }
 }
--- app/src/main/java/com/hafijulislam/doggler/VerticalListActivity.kt
+++ app/src/main/java/com/hafijulislam/doggler/VerticalListActivity.kt
@@ -2,10 +2,24 @@ package com.hafijulislam.doggler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.hafijulislam.doggler.adapters.DogCardAdapter
+import com.hafijulislam.doggler.models.Dog
+import com.hafijulislam.doggler.data.DataSource
 
 class VerticalListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_vertical_list)
+
+        val dogs: List<Dog> = DataSource.dogs
+
+        val recyclerView: RecyclerView = findViewById(R.id.verticalRecyclerView)
+
+        recyclerView.adapter = DogCardAdapter(this, R.id.verticalRecyclerView)
+
+        recyclerView.setHasFixedSize(true)
     }
 }
