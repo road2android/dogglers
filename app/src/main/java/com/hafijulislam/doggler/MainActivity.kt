b35a3b4c39c5ba02b5546fb9396145569c6ff114
Hafijul Islam <hafijul233@gmail.com>
2023-01-16 11:49:29 +0600

    partial code added

--- app/src/main/java/com/hafijulislam/doggler/MainActivity.kt
+++ app/src/main/java/com/hafijulislam/doggler/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.hafijulislam.doggler
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var listIntent: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
